50eee841417ff07e026d9b7666b30866d86b4caa
Peter Barnum <peterbarnum@gmail.com>
2023-12-07 20:05:30 -0700

    add option to omit newline from `spacerLog`

--- helpers/testing.js
+++ helpers/testing.js
@@ -2,8 +2,12 @@ const c = require('cli-color');
 
 const passLog = str => console.log(c.greenBright(`${str} => Pass`));
 const failLog = str => console.log(c.redBright(`${str} => Fail`));
-const spacerLog = (str = '') =>
-  console.log(c.magentaBright(`\n-----------${str || ''}-----------\n`));
+const spacerLog = (str = '', newline = true) =>
+  console.log(
+    c.magentaBright(
+      `\n-----------${str || ''}-----------${newline ? '\n' : ''}`
+    )
+  );
 const titleLog = (str = '', newline = true) =>
   console.log(`${newline ? '\n' : ''}${c.yellowBright.bold.underline(str)}`);
 const testLog = (result, expectedResult) =>
